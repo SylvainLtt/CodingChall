bec4ee0edf5afdab319af7eee2526ab8698a92d7
Sylvain LOTOUT <sylvain.lotout@neo-soft.fr>
2020-02-14 13:22:06 +0100

    add api route

--- app/app.js
+++ app/app.js
@@ -1,4 +1,13 @@
-var reading_digits = require('./reading_digits');
+const express = require('express')
+const app = express()
 
+const reading_digits = require('../app/reading_digits');
 
-console.log(reading_digits.repeatedLookAndSay("1", 5));
+app.get('/api/lookandsay/:input', function (req, res) {
+    let result = reading_digits.repeatedLookAndSay(req.params.input, req.query.times ? req.query.times : 1)
+    res.send({next_string: result});
+})
+
+app.listen(3000, function () {
+    console.log('Started express on port 3000')
+  })
